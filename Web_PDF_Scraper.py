45fde11975b0d18be91136805f251b3d2114d932
kchen79427 <60418935+kchen79427@users.noreply.github.com>
2022-04-04 21:03:15 -0700

    scrapers update

---
+++ Web_PDF_Scraper.py
@@ -0,0 +1,20 @@
+# download pdf files from a hosted site
+
+import os
+import requests
+from urllib.parse import urljoin
+from bs4 import BeautifulSoup
+
+url = r"https://tobychristie.com/loop-data-2021-nascar-cup-series/"
+
+#If there is no such folder, the script will create one automatically
+folder_location = r'C:\Users\kchen\OneDrive\MADS\SIADS591_592_Milestone1\Milestone_I\data\loop_TobyChristie\pdf\2021'
+if not os.path.exists(folder_location):os.mkdir(folder_location)
+
+response = requests.get(url)
+soup= BeautifulSoup(response.text, "html.parser")     
+for link in soup.select("a[href$='.pdf']"):
+    #Name the pdf files using the last portion of each link which are unique in this case
+    filename = os.path.join(folder_location,link['href'].split('/')[-1])
+    with open(filename, 'wb') as f:
+        f.write(requests.get(urljoin(url,link['href'])).content)
